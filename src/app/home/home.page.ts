781dd624487ebce666e8dd937e7808b7380f02db
attrix <lucho.bernal@hotmail.com>
2021-09-16 15:26:12 -0300

    setGoogleAPI

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+
+declare var google;
+interface Marker {
+  position: {
+    lat: number,
+    lng: number,
+  };
+  title: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -7,6 +17,66 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
 
+  actualPos: any = { lat: 0, lng: 0 };
+
+  map = null;
+  markers: any[] = [
+    {
+      position: {
+        lat: 4.658383846282959,
+        lng: -74.09394073486328,
+      },
+      title: 'Parque Simón Bolivar'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    // create a new map by passing HTMLElement
+    const mapEle: HTMLElement = document.getElementById('map');
+    // create LatLng object
+    const myLatLng = { lat: this.actualPos.lat, lng: this.actualPos.lng };
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 12
+    });
+
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      this.renderMarkers();
+      mapEle.classList.add('show-map');
+    });
+  }
+
+  renderMarkers() {
+    this.markers.forEach(marker => {
+      this.addMarker(marker);
+    });
+  }
+
+  addMarker(marker: Marker) {
+    return new google.maps.Marker({
+      position: marker.position,
+      map: this.map,
+      title: marker.title
+    });
+  }
+
+  showLocation() {
+
+    const printCurrentPosition = async () => {
+      const coordinates = await Geolocation.getCurrentPosition();
+      this.actualPos.lat = coordinates.coords.latitude;
+      this.actualPos.lng = coordinates.coords.longitude;
+    }
+    console.log(this.actualPos);
+    printCurrentPosition();
+    this.loadMap();
+  }
 }
